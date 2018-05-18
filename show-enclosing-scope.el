f9dcbe2d7636acedcf60ecb41baa1b3b4650e9a7
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2018-05-17 19:37:59 -0500

    show-enclosing-scope: remove (redisplay) and (goto-char) calls

--- show-enclosing-scope.el
+++ show-enclosing-scope.el
@@ -12,31 +12,23 @@ scopes based on indentation.")
 
 (defvar show-enclosing-scope--window nil)
 
-(setq show-enclosing-scope--scroll t)
+(setq show-enclosing-scope--scroll-down-amount 0)
 
 (defun show-enclosing-scope--delete-window ()
   "Kill the minimap window."
-  (let ((scroll-down-amount 0))
-    (when (window-live-p show-enclosing-scope--window)
-      (let* ((partner (window-parameter
-                       show-enclosing-scope--window 'show-enclosing-scope-partner))
-             (margin1 (window-margins partner))
-             (margin2 (window-margins show-enclosing-scope--window))
-             (fringe1 (window-fringes partner))
-             (fringe2 (window-fringes show-enclosing-scope--window))
-	     (point-pos (point)))
-	(dotimes (i (window-total-height show-enclosing-scope--window))
-	  (condition-case nil
-	      (when show-enclosing-scope--scroll (scroll-down 1))
-	    (error
-	     (incf scroll-down-amount))))
-	(redisplay)
-	(when show-enclosing-scope--scroll (goto-char point-pos))
-	(delete-window show-enclosing-scope--window)
-	(set-window-margins partner (car margin1) (cdr margin2))
-	(set-window-fringes partner (car fringe1) (cadr fringe2))
-	(setq show-enclosing-scope--window nil)))
-    scroll-down-amount))
+  (when (window-live-p show-enclosing-scope--window)
+    (let* ((partner (window-parameter
+                     show-enclosing-scope--window 'show-enclosing-scope-partner))
+           (margin1 (window-margins partner))
+           (margin2 (window-margins show-enclosing-scope--window))
+           (fringe1 (window-fringes partner))
+           (fringe2 (window-fringes show-enclosing-scope--window))
+	   (point-pos (point)))
+      (setq show-enclosing-scope--scroll-down-amount (window-total-height show-enclosing-scope--window))
+      (delete-window show-enclosing-scope--window)
+      (set-window-margins partner (car margin1) (cdr margin2))
+      (set-window-fringes partner (car fringe1) (cadr fringe2))
+      (setq show-enclosing-scope--window nil)))
 
 (defun show-enclosing-scope--get-buffer ()
   (get-buffer-create "show-enclosing-scope"))
@@ -44,16 +36,14 @@ scopes based on indentation.")
 (defun show-enclosing-scope--split-window (size)
   "Make a minimap window."
   ;; make sure that the old one is killed
-  (let* ((scroll-down-amount (show-enclosing-scope--delete-window))
-	 (basewin (selected-window))
+  (show-enclosing-scope--delete-window)
+  ;; split new one off
+  (let* ((basewin (selected-window))
          (margin (window-margins basewin))
          (fringe (window-fringes basewin))
-	 (point-pos (point)))
-    ;; split new one off
-    (condition-case nil
-	(when show-enclosing-scope--scroll (scroll-up (- window-size scroll-down-amount)))
-      (error nil))
-    (redisplay)
+	 (point-linenum (line-number-at-pos))
+	 (scroll-amount (- size show-enclosing-scope--scroll-down-amount))
+	 (window-start-linenum (+ (line-number-at-pos (window-start)) scroll-amount)))
     (set-window-margins basewin (car margin) 0)
     (set-window-fringes basewin (car fringe) 0)
     (let ((win (split-window
@@ -62,8 +52,10 @@ scopes based on indentation.")
       (set-window-margins win 0 (cdr margin))
       (set-window-fringes win 0 (cadr fringe))
       (setq show-enclosing-scope--window win)
-      (set-window-buffer win buf)
-      (when show-enclosing-scope--scroll (goto-char point-pos)))))
+      (set-window-buffer win buf))
+    (if (> window-start-linenum point-linenum)
+	(recenter)
+      (scroll-up scroll-amount))))
 
 (defun show-enclosing-scope--default-list-scopes-recurse (prev-column k)
   (unless (or
@@ -89,6 +81,7 @@ scopes based on indentation.")
     (if (< 1 window-size)
 	(show-enclosing-scope--split-window window-size)
       (show-enclosing-scope--delete-window))
+    (setq show-enclosing-scope--scroll-down-amount 0)
     (with-current-buffer (show-enclosing-scope--get-buffer)
       (erase-buffer)
       (setq mode-line-format nil)
@@ -100,6 +93,8 @@ scopes based on indentation.")
 
 (add-hook 'pre-command-hook 'show-enclosing-scope--delete-window nil :local)
 (add-hook 'post-command-hook 'show-enclosing-scope--refresh nil :local)
+
+()
 
 ;; (remove-hook 'pre-command-hook 'show-enclosing-scope--delete-window)
 ;; (remove-hook 'post-command-hook 'show-enclosing-scope--refresh)
